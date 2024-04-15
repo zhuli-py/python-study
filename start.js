976c60786389ddac6aaf5d979c1344ff26b5c3c5
zhuli-py <1527877980@qq.com>
2024-04-15 12:01:24 +0800

    停止任务实例

---
+++ start.js
@@ -0,0 +1,31 @@
+async function start(startId,endId){
+    if(!startId || !endId) return 
+    let nowId = startId
+    while (nowId < endId) {
+        await stopJob(nowId)
+        nowId++
+    }
+}
+
+function stopJob(id){
+    return fetch("http://172.16.2.220:9003/dolphinscheduler/projects/zl_test2/executors/execute", {
+        "body": "processInstanceId="+id+"&executeType=STOP",
+        "cache": "default",
+        "credentials": "omit",
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "digi-middleware-auth-app": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImRtcCIsImluc2lkZSI6ZmFsc2UsInNpZCI6NDgzOTczNDM2NDQ1MjQ4fQ.qwhKIOpZzHE1k8rWa77tdOTtUaRVFjuQYYxx4WncfgE",
+            "digi-middleware-auth-user": "6edfd306-09f6-4a0f-968b-3ffe5e9a3dcb",
+            "digi-middleware-tenant-id": "649294760776256",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"
+        },
+        "method": "POST",
+        "mode": "cors",
+        "redirect": "follow",
+        "referrer": "http://172.16.2.220:9003/",
+        "referrerPolicy": "strict-origin-when-cross-origin"
+    })
+}
+start(83882,83932)
--- zl.py
+++ zl.py
@@ -67,29 +67,53 @@
 #!/usr/bin/python3
  
 #类定义
-class people:
-    #定义基本属性
-    name = ''
-    age = 0
-    #定义私有属性,私有属性在类外部无法直接进行访问
-    __weight = 0
-    #定义构造方法
-    def __init__(self,n,a,w):
-        self.name = n
-        self.age = a
-        self.__weight = w
-    def speak(self):
-        print("%s 说: 我 %d 岁。" %(self.name,self.age))
+# class people:
+#     #定义基本属性
+#     name = ''
+#     age = 0
+#     #定义私有属性,私有属性在类外部无法直接进行访问
+#     __weight = 0
+#     #定义构造方法
+#     def __init__(self,n,a,w):
+#         self.name = n
+#         self.age = a
+#         self.__weight = w
+#     def speak(self):
+#         print("%s 说: 我 %d 岁。" %(self.name,self.age))
  
-#单继承示例
-class student(people):
-    grade = ''
-    def __init__(self,n,a,w,g):
-        #调用父类的构函
-        people.__init__(self,n,a,w)
-        self.grade = g
-    #覆写父类的方法
-    def speak(self):
-        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
-s = student('ken',10,60,3)
-s.speak()
+# #单继承示例
+# class student(people):
+#     grade = ''
+#     def __init__(self,n,a,w,g):
+#         #调用父类的构函
+#         people.__init__(self,n,a,w)
+#         self.grade = g
+#     #覆写父类的方法
+#     def speak(self):
+#         print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
+# s = student('ken',10,60,3)
+# s.speak()
+
+
+#!/usr/bin/python3
+ 
+# class Parent:        # 定义父类
+#    def myMethod(self):
+#       print ('调用父类方法')
+ 
+# class Child(Parent): # 定义子类
+#    def myMethod(self):
+#       print ('调用子类方法')
+ 
+# c = Child()          # 子类实例
+# c.myMethod()         # 子类调用重写方法
+# super(Child,c).myMethod() #用子类对象调用父类已被覆盖的方法
+
+#调用子方法
+
+
+def test():
+    print("test")
+
+       
+            
